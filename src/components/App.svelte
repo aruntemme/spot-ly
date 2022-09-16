617b99a6593ca6fe9e94cb8e9836ff1454c7020e
aruntemme <arungauthamk@gmail.com>
2022-09-16 21:36:29 +0530

    initial commit 🚀

---
+++ src/components/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import You from './You.svelte';
+    import { Router, Link, Route } from "svelte-routing";
+    export let url = "";
+</script>
+
+<Router url="{url}">
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <Link class="navbar-brand" to="/home">
+                Spot-ly
+            </Link>
+        </div>
+      </nav>
+      <Route path="/you" component={You} />
+</Router>
---
+++ src/components/You.svelte
@@ -0,0 +1,25 @@
+<div>
+    <div class="container d-flex flex-column justify-content-center align-items-center pt-5">
+        <div class="mb-3  w-70 w-md-35">
+            <img src="https://scannables.scdn.co/uri/plain/png/000000/white/640/spotify:track:{trackId}" class="img-fluid" alt="Spotify">
+
+            <button type="button" class="btn btn-outline-primary mt-2 w-100" on:click="{shuffleTracks}">
+                Shuffle
+            </button>
+        </div>
+    </div>
+</div>
+
+<script>
+    import { navigate } from 'svelte-routing';
+    const albumListsIds = [
+        '6R0FwF48PO2BrdZpLeWi42',
+        '0r1ppERo8Mp5iKg3lANvnB',
+        '58UL0Wq1AOKkwEaLTTGMxa'
+    ]
+    let trackId = albumListsIds[0];
+
+    const shuffleTracks = () => {
+        trackId = albumListsIds[Math.floor(Math.random() * albumListsIds.length)];
+    }
+</script>
