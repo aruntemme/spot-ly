342ba2c218b954949aa9e3fd0702ef6ffbf25a94
aruntemme <arungauthamk@gmail.com>
2022-09-16 21:51:11 +0530

    fix

--- src/components/You.svelte
+++ src/components/You.svelte
@@ -1,6 +1,8 @@
 <div>
     <div class="container d-flex flex-column justify-content-center align-items-center pt-5">
         <div class="mb-3  w-70 w-md-35">
+            <p>Hi Leela, Arun missing you ♥️</p>
+            
             <img src="https://scannables.scdn.co/uri/plain/png/000000/white/640/spotify:track:{trackId}" class="img-fluid" alt="Spotify">
 
             <button type="button" class="btn btn-outline-primary mt-2 w-100" on:click="{shuffleTracks}">
